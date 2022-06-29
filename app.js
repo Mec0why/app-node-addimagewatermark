ff4ebccb2bba9a36ce24361f027c25c7eba01c37
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-06-29 20:35:58 +0200

    Add startApp function

--- app.js
+++ app.js
@@ -30,4 +30,30 @@ const addImageWatermarkToImage = async function (
   await image.quality(100).writeAsync(outputFile);
 };
 
+const startApp = async () => {
 
+    // Ask if user is ready
+    const answer = await inquirer.prompt([{
+        name: 'start',
+        message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+        type: 'confirm'
+      }]);
+  
+    // if answer is no, just quit the app
+    if(!answer.start) process.exit();
+  
+    // ask about input file and watermark type
+    const options = await inquirer.prompt([{
+      name: 'inputImage',
+      type: 'input',
+      message: 'What file do you want to mark?',
+      default: 'test.jpg',
+    }, {
+      name: 'watermarkType',
+      type: 'list',
+      choices: ['Text watermark', 'Image watermark'],
+    }]);
+  
+  }
+  
+  startApp();
